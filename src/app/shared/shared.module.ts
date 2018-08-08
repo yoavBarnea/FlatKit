9da8926121064292fdb7da6c0445eb500b375b68
Yoav Barnea <barnea.mail@gmail.com>
2018-08-08 12:41:36 +0300

    adding a shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModule
+  ],
+  declarations: [],
+  exports: [
+    CommonModule,
+    FormsModule,
+    NgbModule
+  ]
+
+})
+export class SharedModule { }
