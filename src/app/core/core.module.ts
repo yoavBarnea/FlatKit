00ade665e4b4068d2099e755935a6a970ae5e584
Yoav Barnea <barnea.mail@gmail.com>
2018-08-10 15:35:19 +0300

    added AuthInterceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from "@angular/common/http"
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 
-import { CoreRoutingModule } from './core-routing.module';
-import { HomeComponent } from './home/home.component';
-import { AuthService } from './services/auth.service';
-import { AuthGuard } from './services/auth.guard';
+import { CoreRoutingModule } from "./core-routing.module";
+import { HomeComponent } from "./home/home.component";
+import { AuthService } from "./services/auth.service";
+import { AuthGuard } from "./services/auth.guard";
+import { httpInterceptorProviders } from "./interceptors/interceptors-providers";
+
 @NgModule({
-  imports: [
-    CommonModule,
-    CoreRoutingModule,
-    HttpClientModule
-  ],
+  imports: [CommonModule, CoreRoutingModule, HttpClientModule],
   declarations: [HomeComponent],
-  providers: [AuthService, AuthGuard]
+  providers: [AuthService, AuthGuard, httpInterceptorProviders]
 })
-export class CoreModule { }
+export class CoreModule {}
---
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -0,0 +1,21 @@
+
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './../services/auth.service';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+   const authReq = req.clone({
+     setHeaders: { Autorization : this.authService.token }
+   });
+
+    return next.handle(authReq);
+  }
+}
+
